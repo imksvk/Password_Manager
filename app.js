8964e7de4f3d5fe12a46a90e56f660383a139132
imksvk <keshavkumar123456789101112@gmail.com>
2025-03-08 10:22:04 +0530

    minor updates

--- app.js
+++ app.js
@@ -1,18 +1,62 @@
-
 let btn = document.querySelector("#submitbtn");
+let tb = document.querySelector("table");
 
-btn.addEventListener("click",(e)=>{
+// const deletepassword = (website)=>{
+//     let data = localStorage.getItem("passwords");
+//     let arr = JSON.parse(data);
+
+
+// }
+
+
+const showpasswords = () => {
+    let data = localStorage.getItem("passwords");
+    if (data == null) {
+        tb.innerHTML = "No Data to show";
+    } else {
+        tb.innerHTML = `<tr>
+                <th>website</th>
+                <th>Username</th>
+                <th>Passwords</th>
+                <th>Delete</th>
+            </tr>`;
+        let arr = JSON.parse(data);
+        for (let i = 0; i < arr.length; i++) {
+            const element = arr[i];
+            let str = `<tr>
+                        <td>${element.website}</td>
+                        <td>${element.username}</td>
+                        <td>${element.password}</td>
+                        <td>${"Delete"}</td>
+                    </tr>`
+            tb.innerHTML += str;
+        }
+    }
+    // logic
+
+}
+
+showpasswords();
+
+btn.addEventListener("click", (e) => {
     e.preventDefault();
     let passwords = localStorage.getItem("passwords");
-    if(!passwords){
+    if (!passwords) {
         let json = [];
-        json.push({username:username,password: password});
+        let username = document.querySelector("#username").value;
+        let password = document.querySelector("#password").value;
+        let website = document.querySelector("#website").value;
+        json.push({ website: website, username: username, password: password });
         alert("password saved");
-        localStorage.setItem("passwords",JSON.stringify(json));
-    }else{
+        localStorage.setItem("passwords", JSON.stringify(json));
+    } else {
         let json = JSON.parse(localStorage.getItem("passwords"));
-        json.push({username:username,password: password});
+        let username = document.querySelector("#username").value;
+        let password = document.querySelector("#password").value;
+        let website = document.querySelector("#website").value;
+        json.push({ website: website, username: username, password: password });
         alert("password saved");
-        localStorage.setItem("passwords",JSON.stringify(json));
+        localStorage.setItem("passwords", JSON.stringify(json));
     }
+    showpasswords();
 })
